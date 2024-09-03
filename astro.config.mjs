11f5a1bd50c2630e35613b5ed548f31d30887c36
vanGamo <vangamo.beta@gmail.com>
2024-09-03 20:12:13 +0200

    Add new package to list repo posts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import listDirectory from 'astro-list-directory-integration';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), listDirectory()]
 });
