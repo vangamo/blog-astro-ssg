62d5d3295b29fb84ed533bf6a49f09b69716765a
vanGamo <vangamo.beta@gmail.com>
2024-09-04 11:03:04 +0200

    Remove package to list repo posts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import listDirectory from 'astro-list-directory-integration';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), listDirectory()]
+  integrations: [tailwind()]
 });
