8835a029a38138b36d32d0ec70da076524aafb99
Van Gamo <vangamo.beta@gmail.com>
2024-10-07 20:07:40 +0200

    Add i18n configuration. Customize header links.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", {codes: ["en", "en_US", "en_UK"], path: "english"}],
+  },
   experimental: {
     contentLayer: true,
   },
