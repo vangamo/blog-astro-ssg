6370b94ca7f16341c95a9ea83a552f7d690d7233
vanGamo <vangamo.beta@gmail.com>
2024-12-05 12:29:39 +0100

    Add complete frontmatter schema.

--- src/content/config.js
+++ src/content/config.js
@@ -21,27 +21,74 @@ const generateId = ({ entry, base, data }) => {
 
 const links = defineCollection({
   // The ID is a slug generated from the path of the file relative to `base`
-  loader: glob({ pattern: '**/!(README).md', base: './content/links', generateId: generateId }),
-  type: 'content', /* they'll be Markdown files ('data' for yaml files) */
+  loader: glob({
+    pattern: '**/!(README).md',
+    base: './content/links',
+    generateId: generateId,
+  }),
+  /*type: 'content', they'll be Markdown files ('data' for yaml files) */
   schema: z.object({
     title: z.string(),
     author: z.string(),
     tags: z.array(z.string()),
-    itineraries: z.array(z.string()),
-    status: z.enum(['draft', 'published']),
-  })
+    status: z.enum([
+      'draft',
+      'published',
+      'published-no-index',
+      'published-to-complete',
+      'notes',
+      'scheduled',
+    ]),
+    date: z.object({
+      created: z.string(),
+      edited: z.string().optional(),
+      published: z.string().optional(),
+    }),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    brief: z.string(),
+    link: z.string().url(),
+    origin: z.string(),
+  }),
 });
 
 const posts = defineCollection({
   // The ID is a slug generated from the path of the file relative to `base`
-  loader: glob({ pattern: '**/!(README).md', base: './content/posts', generateId: generateId }),
+  loader: glob({
+    pattern: '**/!(README).md',
+    base: './content/posts',
+    generateId: generateId,
+  }),
   schema: z.object({
     title: z.string(),
     author: z.string(),
     tags: z.array(z.string()),
-    itineraries: z.array(z.string()),
-    status: z.enum(['draft', 'published']),
-  })
+    status: z.enum([
+      'draft',
+      'published',
+      'published-no-index',
+      'published-to-complete',
+      'notes',
+      'scheduled',
+    ]),
+    date: z.object({
+      created: z.string(),
+      edited: z.string().optional(),
+      published: z.string().optional(),
+    }),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    brief: z.string(),
+    'related-posts': z.array(z.string()).optional(),
+  }),
 });
 
 export const collections = { links, posts };
